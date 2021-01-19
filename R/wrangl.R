d64a4aa63a2d4e307e4edfd819c4dafce92b6c0a
andrew <arenn@design.upenn.edu>
2021-01-18 20:37:12 -0500

    Interpolation

--- R/wrangl.R
+++ R/wrangl.R
@@ -222,3 +222,71 @@ windows <-
   theme_map()
 
 anim_save(windows, filename = "gwr.gif", fps = 3)
+
+## grab data from around each cell
+nn_interpolate <- function(data, depth = 5) {
+  
+  play <-
+    data %>%
+    st_transform(2163) %>% 
+    arrange(Y, X) %>%
+    rownames_to_column() %>%
+    mutate(rowname = as.numeric(rowname)) %>%
+    select(-X, -Y) %>%
+    st_as_sf() 
+  
+  crosswalk <- 
+    play %>% 
+    transmute(id = rowname, 
+              GEOID)
+    
+  frst_degree <- 
+    play %>% 
+    st_touches() %>% 
+    tibble() %>%
+    rownames_to_column() %>%
+    clean_names() %>%
+    unnest(x) %>%
+    transmute(row_id = as.numeric(rowname),
+              col_id = x)
+  
+  scnd_degree <- frst_degree %>% 
+    rename(id = row_id,
+           row_id = col_id) %>%
+    left_join(frst_degree) 
+  
+  for (i in 1:depth) {
+    
+    scnd_degree <-
+      scnd_degree %>%
+      transmute(id = id,
+                row_id = col_id) %>%
+      left_join(frst_degree)
+    
+  }
+  
+  final <-
+    scnd_degree %>%
+    transmute(id = id,
+              rowname = col_id) %>%
+    group_by(id) %>%
+    distinct(rowname, .keep_all = TRUE) %>%
+    ungroup() %>%
+    left_join(play) %>%
+    st_as_sf() %>% 
+    select(-rowname) %>%
+    st_drop_geometry() %>%
+    group_by(id, year) %>%
+    summarise_if(is.numeric, ~mean(.x, na.rm = TRUE)) %>% 
+    ungroup() %>%
+    left_join(crosswalk) %>%
+    select(-id) %>%
+    select(GEOID, everything())
+  
+  return(final)
+  
+}
+
+nn_interpolate(full, 3)
+
+
